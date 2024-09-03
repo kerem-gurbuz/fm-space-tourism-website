ea444ad30ba20a78b5ef2fa6c556acf081011ffa
Kerem Gurbuz <grbz.kerem@gmail.com>
2024-09-03 15:19:09 +0300

    feat: create constants folder for centralized project data

---
+++ src/lib/constants/navigation-links.ts
@@ -0,0 +1,23 @@
+type NavigationLink = {
+  label: string;
+  href: string;
+};
+
+export const NAVIGATION_LINKS: NavigationLink[] = [
+  {
+    label: 'home',
+    href: '/',
+  },
+  {
+    label: 'destination',
+    href: '/destination',
+  },
+  {
+    label: 'crew',
+    href: '/crew',
+  },
+  {
+    label: 'technology',
+    href: '/technology',
+  },
+];
